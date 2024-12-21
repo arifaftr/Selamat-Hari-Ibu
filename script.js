24dbc4954fcf0862485e0d623f570e5f2a1eac42
Arifa Fitra Salima <arifaftr05@gmail.com>
2024-12-21 22:38:10 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 let currentMessageIndex = 0;
 const messages = [
   "Happy Bunda Wardah's Day To a beautiful woman, who is as delicate and as lovely as a flower",
-  "Selamat Hari Ibu! Bunda adalah pahlawan dalam hidup kami!",
+  "Happy Bunda Wardah's Day To a beautiful woman, who is as delicate and as lovely as a flower",
   "Bunda, terima kasih atas semua cinta dan dukungan yang telah engkau berikan.",
   "Selamat Hari Ibu! Semoga hari ini penuh kebahagiaan dan cinta!",
   "Bunda, Engkau adalah yang terbaik! Terima kasih telah selalu ada untuk kami."
